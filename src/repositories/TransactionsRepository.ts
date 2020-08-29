dc5b103d93e212bdf372e768a2415ebb54076019
Renato Martinez Costa <renato.martinez@5aattiva.com.br>
2020-08-28 23:05:17 -0300

    Finalização do Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,5 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
@@ -6,23 +7,57 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+  private balance: Balance;
 
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncomeReduce: number = this.transactions.reduce((totalIncome: number, transaction: Transaction) => {
+      if (transaction.type === 'income') {
+        totalIncome += transaction.value;
+      }
+      return totalIncome;
+    },0);
+
+    const totalOutComeReduce = this.transactions.reduce((totalOutcome: number , transaction: Transaction) => {
+      if (transaction.type === 'outcome') {
+        totalOutcome += transaction.value;
+      }
+      return totalOutcome;
+    },0);
+
+    this.balance = {
+      income: totalIncomeReduce,
+      outcome: totalOutComeReduce,
+      total: totalIncomeReduce - totalOutComeReduce,
+    }
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title, type, value});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
